2fb20beb0b5deb90c47fee76938d322683cf00e9
sachin <tavarsachin@gmail.com>
2019-05-21 00:19:28 +0530

    add signup function

--- main.java
+++ main.java
@@ -20,10 +20,11 @@ class Option{
         Scanner readobj = new Scanner(System.in);
         System.out.println("Please Select Operation\n1. Login\n2. Register");
         int op = readobj.nextInt();
+        UserOption login = new UserOption();
         if (op == 1){
-            Login login = new Login(op);
+            login.Login(ltype);
         }else if(op == 2){
-            System.out.println("your reigstering");
+            login.SignUp(ltype);
         }else{
             System.out.println("invald option");
         }
@@ -31,8 +32,8 @@ class Option{
 }
 
 
-class Login{
-	Login(int type){
+class UserOption{
+	public void Login(int type){
         System.out.println("login type is "+type);
         System.out.println("------------Option----------");
         Scanner readobj = new Scanner(System.in);
@@ -42,7 +43,13 @@ class Login{
         String pass = readobj.nextLine();
         //System.out.println("username is "+username+" and password is "+pass);
         Users users = new Users();
-        users.getUser(username, pass);
+        //users.getUser(username, pass);
+    }
+
+    public void SignUp(int type){
+        Scanner readobj = new Scanner(System.in);
+        System.out.println("Welcome to Register page\nPlease enter name");
+        String name = readobj.nextLine();
     }
 }
 
@@ -50,17 +57,17 @@ class Login{
 class Users{
     Map<String, String> user = new HashMap<String, String>();
     Users(){
-        user.put("sachin", "pass");
-        user.put("admin", "admin123");
+        user.put("sachin", "tavarsachin@gmail.com");
+        user.put("admin", "admin@gmail.cm");
+        System.out.println(user);
     }
 
     public void getUser(String username, String pass){
         if (user.containsKey(username))  
         { 
             String a = user.get(username);
-            System.out.println("entered password is "+pass+" stored pass is "+a); 
-            if (a == pass){
-                System.out.println("password for "+username+" = " + a);
+            if (a.equals(pass)){
+                System.out.println("Login Succesfully");
             }else{
                 System.out.println("incorrect password");
             } 
