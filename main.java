a4451c09ebf6e741364cd8cf63f58671bca1e603
sachin <tavarsachin@gmail.com>
2019-05-24 00:13:18 +0530

    loop, logout, add dish

--- main.java
+++ main.java
@@ -5,19 +5,24 @@ import java.text.SimpleDateFormat;
 class Main{
     
 	public static void main(String args[]){
-        System.out.println("Please Select Account type\n1. Customer\n2. Owner\n3. Admin");
-        Scanner readobj = new Scanner(System.in);
-		Integer at;
-		at = readobj.nextInt();
-        Option op = new Option(at);
+        Option op = new Option();
+        op.LoginType();
     }
 }
 
 
 class Option{
     int ltype;
+    
+    public void LoginType(){
+    	System.out.println("\nPlease Select Account type\n1. Customer\n2. Owner\n3. Admin");
+        Scanner readobj = new Scanner(System.in);
+		Integer at;
+		at = readobj.nextInt();
+		SelectOption(at);
+    }
 
-	Option(int ltype){
+	public void SelectOption(int ltype){
         Users users = new Users();
         if(ltype == 3){
             users.Login(ltype);
@@ -47,10 +52,33 @@ class Users{
         user.get("sachin@gmail.com").put("password", "admin123");
         user.get("sachin@gmail.com").put("address", "kondli delhi");
         user.get("sachin@gmail.com").put("type", "1");
+        user.put("kartik@gmail.com", new HashMap<String, String>());
+        user.get("kartik@gmail.com").put("name", "Kartik Tomar");
+        user.get("kartik@gmail.com").put("password", "kartik123");
+        user.get("kartik@gmail.com").put("address", "44, Clayton, vic, 3168");
+        user.get("kartik@gmail.com").put("type", "1");
+        user.put("anand@gmail.com", new HashMap<String, String>());
+        user.get("anand@gmail.com").put("name", "Anand Prakash");
+        user.get("anand@gmail.com").put("password", "anand123");
+        user.get("anand@gmail.com").put("address", "44, Clayton, vic, 3168");
+        user.get("anand@gmail.com").put("type", "1");
+        user.put("sumit@gmail.com", new HashMap<String, String>());
+        user.get("sumit@gmail.com").put("name", "Sumit Garg");
+        user.get("sumit@gmail.com").put("password", "sumit123");
+        user.get("sumit@gmail.com").put("address", "12, Duncan rd, newpoint, vic, 3122");
+        user.get("sumit@gmail.com").put("type", "1");
         user.put("owner@gmail.com", new HashMap<String, String>());
         user.get("owner@gmail.com").put("name", "owner sachin");
         user.get("owner@gmail.com").put("password", "admin123");
         user.get("owner@gmail.com").put("type", "2");
+        user.put("carla@gmail.com", new HashMap<String, String>());
+        user.get("carla@gmail.com").put("name", "Carla Bruni");
+        user.get("carla@gmail.com").put("password", "carla123");
+        user.get("carla@gmail.com").put("type", "2");
+        user.put("raghav@gmail.com", new HashMap<String, String>());
+        user.get("raghav@gmail.com").put("name", "Raghav Sethi");
+        user.get("raghav@gmail.com").put("password", "raghav123");
+        user.get("raghav@gmail.com").put("type", "2");
         user.put("admin", new HashMap<String, String>());
         user.get("admin").put("password", "admin123");
         user.get("admin").put("type", "3");
@@ -109,19 +137,27 @@ class Users{
 			}
 		}
 
-        while(true){
-            System.out.print("\nPlease Enter User email for delete : ");
-            Scanner Userop = new Scanner(System.in);
-		    String email = Userop.nextLine();
-            if(checkUser(email)){
-                user.remove(email);
-                System.out.println("Deleted user "+email);
-                break;
-            }else{
-                System.out.println("User Does not exist");
-            }
-        }
-
+        System.out.println("\n1. Delete User\n2. Logout\n");
+        Scanner dltop = new Scanner(System.in);
+		Integer dlop = dltop.nextInt();
+		if(dlop == 1){
+		    while(true){
+		        System.out.print("\nPlease Enter User email for delete : ");
+		        Scanner Userop = new Scanner(System.in);
+				String email = Userop.nextLine();
+		        if(checkUser(email)){
+		            user.remove(email);
+		            System.out.println("Deleted user "+email);
+		            getUsers();
+		        }else{
+		            System.out.println("User Does not exist");
+		        }
+		    }
+		}else{
+			setLoggedUser(null);
+        	Option op = new Option();
+        	op.LoginType();
+		}
 
     }
 
@@ -179,37 +215,62 @@ class Restaurant{
     public static final HashMap<Integer, HashMap<String, String>> rst = new HashMap<Integer, HashMap<String, String>>();
     public static final HashMap<Integer, HashMap<String, String>> dish = new HashMap<Integer, HashMap<String, String>>();
     public static final HashMap<Integer, Integer> cart = new HashMap<Integer, Integer>();
+    public static Integer itemnumber = 11;
     Scanner readobj = new Scanner(System.in);
 	Users users = new Users();
     Restaurant(){
         rst.put(1, new HashMap<String, String>());
-        rst.get(1).put("name", "kesar");
-        rst.get(1).put("address", "delhi india");
-        rst.get(1).put("contact", "11120392");
+        rst.get(1).put("name", "Derby Thai");
+        rst.get(1).put("address", "4, Derby Rd, Caulfield East, VIC, 3145");
+        rst.get(1).put("owner", "owner@gmail.com");
         rst.put(2, new HashMap<String, String>());
-        rst.get(2).put("name", "haldi ram");
-        rst.get(2).put("address", "noida india");
-        rst.get(2).put("contact", "222392");
+        rst.get(2).put("name", "KFC");
+        rst.get(2).put("address", "13-14, Sir John Monash Dr, Caulfield, VIC, 3145");
+        rst.get(2).put("owner", "owner@gmail.com");
         rst.put(3, new HashMap<String, String>());
-        rst.get(3).put("name", "bikaner");
-        rst.get(3).put("address", "gurgaon india");
-        rst.get(3).put("contact", "3333234234");
+        rst.get(3).put("name", "EC Kitchen");
+        rst.get(3).put("address", "1 Sir John Monash Dr, Caulfield East, VIC, 3145");
+        rst.get(3).put("owner", "owner@gmail.com");
         dish.put(1, new HashMap<String, String>());
-        dish.get(1).put("name", "chilly potato");
-        dish.get(1).put("price", "50");
+        dish.get(1).put("name", "Chilly Potato");
+        dish.get(1).put("price", "10");
         dish.get(1).put("rst", "1");
         dish.put(2, new HashMap<String, String>());
-        dish.get(2).put("name", "chhole bhature");
-        dish.get(2).put("price", "20");
+        dish.get(2).put("name", "Fried Rice");
+        dish.get(2).put("price", "8");
         dish.get(2).put("rst", "1");
         dish.put(3, new HashMap<String, String>());
-        dish.get(3).put("name", "pizza");
-        dish.get(3).put("price", "60");
-        dish.get(3).put("rst", "2");
+        dish.get(3).put("name", "Tofu");
+        dish.get(3).put("price", "6");
+        dish.get(3).put("rst", "1");
         dish.put(4, new HashMap<String, String>());
-        dish.get(4).put("name", "pasta");
-        dish.get(4).put("price", "40");
-        dish.get(4).put("rst", "3");
+        dish.get(4).put("name", "Burger");
+        dish.get(4).put("price", "5");
+        dish.get(4).put("rst", "2");
+        dish.put(5, new HashMap<String, String>());
+        dish.get(5).put("name", "Chicken");
+        dish.get(5).put("price", "6");
+        dish.get(5).put("rst", "2");
+        dish.put(6, new HashMap<String, String>());
+        dish.get(6).put("name", "Fries");
+        dish.get(6).put("price", "4");
+        dish.get(6).put("rst", "2");
+        dish.put(7, new HashMap<String, String>());
+        dish.get(7).put("name", "Coke");
+        dish.get(7).put("price", "3");
+        dish.get(7).put("rst", "2");
+        dish.put(8, new HashMap<String, String>());
+        dish.get(8).put("name", "Chilly Potato");
+        dish.get(8).put("price", "10");
+        dish.get(8).put("rst", "2");
+        dish.put(9, new HashMap<String, String>());
+        dish.get(9).put("name", "Fried Rice");
+        dish.get(9).put("price", "8");
+        dish.get(9).put("rst", "2");
+        dish.put(10, new HashMap<String, String>());
+        dish.get(10).put("name", "Tofu");
+        dish.get(10).put("price", "6");
+        dish.get(10).put("rst", "2");
     }
     public void chooseRestaurant(){
         System.out.println("\n-------------Please Select Restaurant--------------------");
@@ -218,6 +279,7 @@ class Restaurant{
                System.out.println("\tAddress: "+rst.get(objectName).get("Address")+"\n\tContact : "+rst.get(objectName).get("contact")+"\n");
         }
         System.out.println("----------------------------------------------------------");
+        Scanner readobj = new Scanner(System.in);
         String selectedrst = readobj.nextLine();
         getRestaurantItem(selectedrst);
     }
@@ -240,13 +302,45 @@ class Restaurant{
             setItemToCart(selecteditem, qty, selectedrst);
         }else if(UserType == 2){
             while(true){
-				System.out.print("\nPlease Enter Item Id for deleting : ");
-				Integer itemId = readobj.nextInt();
-				if(ItemExist(itemId)){
-					System.out.println("item deleted");
-					break;
+				System.out.println("\n1. Add Item\n2. Delete Item\n3. Lgout\n");
+				Scanner readitem = new Scanner(System.in);
+				Integer Itemoption = readitem.nextInt();
+				if (Itemoption == 1){
+					Scanner dishobj = new Scanner(System.in);
+					System.out.print("Please Enter Dish Name : ");
+					String name = dishobj.nextLine();
+					System.out.print("Please Enter Dish Price : ");
+					String price = dishobj.nextLine();
+					dish.put(itemnumber, new HashMap<String, String>());
+					dish.get(itemnumber).put("name", name);
+					dish.get(itemnumber).put("price", price);
+					dish.get(itemnumber).put("rst", selectedrst);
+					itemnumber += 1;
+					chooseRestaurant();
+				}else if(Itemoption == 2){
+					Scanner readid = new Scanner(System.in);
+					Integer itemId = readid.nextInt();
+					if(ItemExist(itemId)){
+						dish.remove(itemId);
+						System.out.println("\nitem deleted\n");
+						System.out.println("1. Logout\n2. Select Restaurent");
+						Scanner finalobj = new Scanner(System.in);
+						Integer cont = finalobj.nextInt();
+						if (cont == 1){
+							users.setLoggedUser(null);
+							Option op = new Option();
+							op.LoginType();
+						}else{
+							chooseRestaurant();
+						}
+		                //break;
+					}else{
+						System.out.println("item not exists");
+					}
 				}else{
-					System.out.println("item not exists");
+					users.setLoggedUser(null);
+					Option op = new Option();
+					op.LoginType();	
 				}
 			}
         }
@@ -312,5 +406,14 @@ class Restaurant{
         System.out.println("Address     \t: \t"+users.user.get(users.getLoggedUser()).get("address"));
         System.out.println("Date & Time \t: \t"+dateFormat.format(date));
         System.out.println("\n-----------------End of Invoice Thanks for purchasing-------------------\n\n");
+        System.out.println("1. Logout\n2. Select Restaurent");
+        Integer cont = readobj.nextInt();
+        if (cont == 1){
+        	users.setLoggedUser(null);
+        	Option op = new Option();
+        	op.LoginType();	
+        }else{
+            chooseRestaurant();
+        }
     }
 }
